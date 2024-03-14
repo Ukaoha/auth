b84692fe63ff781fb1d58f987a391ea6103befe2
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-03-14 15:49:03 +0000

    prisma

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -7,6 +7,14 @@ export async function POST(req: Request) {
     const { fullName, email, password } = await req.json();
 
     const hashedPass = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+      data: {
+        fullName,
+        email,
+        password: hashedPass,
+      },
+    });
     return new NextResponse(
       JSON.stringify({
         message: "registration suscessfull",
