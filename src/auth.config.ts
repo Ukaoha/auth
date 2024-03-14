608931c120e56a04802ea3458c68b0e21a3c7fcf
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-03-14 16:42:01 +0000

    auth

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,31 +1,34 @@
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "./schema";
+import { signinUser } from "./user";
+// import 
 
-export const authConfig = {
-  pages: {
-    signIn: "/login",
-  },
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard", nextUrl));
-      }
-      return true;
-    },
-  },
+export default {
   providers: [
     Credentials({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await signinUser({ email, password });
+          if (!user) return null;
+
+          return user.res;
+        }
+        return null;
       },
     }),
-    
   ],
+  callbacks: {
+    async jwt({ user, token }) {
+      if (token) {
+        token.accessToken = token.token!;
+        token.exp = token.exp!;
+      }
+      return token;
+    },
+  },
 } satisfies NextAuthConfig;
